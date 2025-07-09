38792137eac8ad807d2bab539ef091b4430fda83
Александр Приходько <108543198+Racionalistas@users.noreply.github.com>
2025-07-09 18:22:16 +0300

    Update autosendScript.js

--- autosendScript.js
+++ autosendScript.js
@@ -2,7 +2,7 @@ let coverLetter = `...`;
 
 const linkWithFilters = `...`;
 
-let delayTimeMS = 700;
+let delayTimeMS = 800;
 
 function triggerInputChange(node, value = '') {
   const inputTypes = [
@@ -13,12 +13,23 @@ function triggerInputChange(node, value = '') {
 
   if (inputTypes.includes(node?.__proto__?.constructor)) {
     const valueSetter = Object.getOwnPropertyDescriptor(node.__proto__, 'value').set;
-
-    valueSetter.call(node, value);
+	  valueSetter.call(node, value.replace(/\n/g, '\r\n'));
 
     const event = new Event('input', { bubbles: true });
 
     node.dispatchEvent(event);
+  }
+}
+
+async function handleAdditionalCoverLetterClick() {
+  // Задержка перед нажатием, чтобы попап успел открыться
+  await delay(delayTimeMS);
+  await delay(delayTimeMS);
+
+  const addCoverLetterBtn = document.querySelector('[data-qa="add-cover-letter"]');
+  
+  if (addCoverLetterBtn) {
+      addCoverLetterBtn.click();
   }
 }
 
@@ -30,40 +41,30 @@ async function handlerCoverLetter() {
 
     if (document.querySelector('[data-qa="vacancy-response-letter-toggle"]')) {
         document.querySelector('[data-qa="vacancy-response-letter-toggle"]').click();
-        await delay(50);
+        await delay(150);
 
         messageArea = document.querySelector(".magritte-textarea-native___w-czf_2-0-57");
-        messageArea.textContent = coverLetter;
-
+        triggerInputChange(messageArea, coverLetter);
         btnSubmit = document.querySelector('[data-qa="vacancy-response-letter-submit"]');
     }
     else if (document.querySelector('[data-qa="vacancy-response-popup-form-letter-input"]')) {
         messageArea = document.querySelector('[data-qa="vacancy-response-popup-form-letter-input"]');
         triggerInputChange(messageArea, coverLetter);
-
-        var evt = document.createEvent('HTMLEvents');
-        evt.initEvent('change', true, true);
-        messageArea.dispatchEvent(evt);
-
         btnSubmit = document.querySelector('[data-qa="vacancy-response-submit-popup"]');
     } else {
         messageArea = document.querySelector(".magritte-textarea-native___w-czf_2-0-57");
-        messageArea.textContent = coverLetter;
-
-        var evt = document.createEvent('HTMLEvents');
-        evt.initEvent('change', true, true);
-        messageArea.dispatchEvent(evt);
-
+        triggerInputChange(messageArea, coverLetter);
         btnSubmit = document.querySelector('[data-qa="vacancy-response-submit-popup"]');
     }
 
-    await delay(50);
+    await delay(150);
     btnSubmit.click();
 
     await delay(delayTimeMS);
     window.location.href = linkWithFilters + '&continue=true';
 }
 
+// Остальной ваш код без изменений
 async function autoResponse() {
     let vacancies = document.querySelectorAll('[data-qa="vacancy-serp__vacancy_response"]');
 
@@ -97,6 +98,7 @@ async function autoResponse() {
             window.location.href = linkWithFilters + '&continue=true&delete=true';
         } else {
             console.log('vacancie is ok, proceed to cover letter');
+    		    await handleAdditionalCoverLetterClick();
             await handlerCoverLetter();
         }
     } else {
@@ -107,13 +109,17 @@ async function autoResponse() {
             vacancy.click();
 
             await delay(delayTimeMS);
+
             if (document.querySelector('[data-qa="employer-asking-for-test"]')) {
                 window.location.href = linkWithFilters + '&continue=true&delete=true';
             } else {
+    			      // Проверка и нажатие "add-cover-letter" после открытия попап
+    			      await handleAdditionalCoverLetterClick();
                 await handlerCoverLetter();
             }
-        }
-        else if (window.location.pathname === '/applicant/vacancy_response') {
+        } else if (window.location.pathname === '/applicant/vacancy_response') {
+            // Проверка и нажатие "add-cover-letter" после открытия попап
+            await handleAdditionalCoverLetterClick();            
             await handlerCoverLetter();
         }
     }
